f2efbf4f8c9fb30cc768211d420d1a1ba5949d4d
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-03 11:57:34 +0100

    Create Clusterscouldnotzoomabelianmodrlsovercole.h

---
+++ Clusterscouldnotzoomabelianmodrlsovercole.h
@@ -0,0 +1,6 @@
+//to be better, we need to start a thread.
+//this thread on one core on cpu must be a eoobi virus.
+//so, we need to refactor letent singals hashed.
+//this would force the cpus to stop other dead threads.
+//the count of the dead threads caanot be set after starting
+//a model shutdiwn.
